66f87320c56b1873dfb08e0055045c9dd4b90af5
FABIEN KOH CHUN KIAT <2202730D@student.tp.edu.sg>
2024-02-02 15:02:27 +0800

    Added REST API Endpoints and Unit Of Work pattern.

---
+++ CarRentalManagement/Server/Controllers/ColoursController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarRentalManagement.Server.Data;
+using CarRentalManagement.Shared.Domain;
+using CarRentalManagement.Server.IRepository;
+
+namespace CarRentalManagement.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ColoursController : ControllerBase
+    {
+        //Refractored
+        //private readonly ApplicationDbContext _context;
+        private readonly IUnitOfWork _unitOfWork;
+
+        //Refractored
+        //public ColoursController(ApplicationDbContext context)
+        public ColoursController(IUnitOfWork unitOfWork)
+        {
+            //Refractored
+            //_context = context;
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Colours
+        [HttpGet]
+        //Refractored
+        //
+        //public async Task<ActionResult<IEnumerable<Colour>>> GetColours()
+        public async Task<IActionResult> Get()
+        {
+            //Refractored
+            //return await _context.Colours.ToListAsync();
+            var Colours = await _unitOfWork.Colours.GetAll();
+            return Ok(Colours);
+        }
+
+        // GET: api/Colours/5
+        [HttpGet("{id}")]
+        //Refractored
+        //public async Task<ActionResult<Colour>> GetColour(int id)
+        public async Task<IActionResult> GetColour(int id)
+        {
+            //Refractored
+            //var Colour = await _context.Colours.FindAsync(id);
+            var Colour = await _unitOfWork.Colours.Get(q => q.Id == id);
+
+            if (Colour == null)
+            {
+                return NotFound();
+            }
+            //Refractored
+            return Ok(Colour);
+        }
+
+        // PUT: api/Colours/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutColour(int id, Colour Colour)
+        {
+            if (id != Colour.Id)
+            {
+                return BadRequest();
+            }
+            //Refractored
+            //_context.Entry(Colour).State = EntityState.Modified;
+            _unitOfWork.Colours.Update(Colour);
+
+            try
+            {
+                //Refractored
+                //await _context.SaveChangesAsync();
+                await _unitOfWork.Save(HttpContext);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                //Refractored
+                //if (!ColourExists(id))
+                if (!await ColourExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Colours
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Colour>> PostColour(Colour Colour)
+        {
+            //Refractor
+            //_context.Colours.Add(Colour);
+            //await _context.SaveChangesAsync();
+            await _unitOfWork.Colours.Insert(Colour);
+            await _unitOfWork.Save(HttpContext);
+
+            return CreatedAtAction("GetColour", new { id = Colour.Id }, Colour);
+        }
+
+        // DELETE: api/Colours/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteColour(int id)
+        {
+            //Refractored
+            //var Colour = await _context.Colours.FindAsync(id);
+            var Colour = await _unitOfWork.Colours.Get(q => q.Id == id);
+            if (Colour == null)
+            {
+                return NotFound();
+            }
+            //Refractored
+            //_context.Colours.Remove(Colour);
+            //await _context.SaveChangesAsync();
+            await _unitOfWork.Colours.Delete(id);
+            await _unitOfWork.Save(HttpContext);
+
+            return NoContent();
+        }
+
+        //Refractored
+        //private bool ColourExists(int id)
+        private async Task<bool> ColourExists(int id)
+        {
+            //Refractored
+            //return (_context.Colours?.Any(e => e.Id == id)).GetValueOrDefault();
+            var Colour = await _unitOfWork.Colours.Get(q => q.Id == id);
+            return Colour != null;
+        }
+    }
+}
---
+++ CarRentalManagement/Server/Controllers/MakesController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarRentalManagement.Server.Data;
+using CarRentalManagement.Shared.Domain;
+using CarRentalManagement.Server.IRepository;
+
+namespace CarRentalManagement.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MakesController : ControllerBase
+    {
+        //Refractored
+        //private readonly ApplicationDbContext _context;
+        private readonly IUnitOfWork _unitOfWork;
+
+        //Refractored
+        //public MakesController(ApplicationDbContext context)
+        public MakesController(IUnitOfWork unitOfWork)
+        {
+            //Refractored
+            //_context = context;
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Makes
+        [HttpGet]
+        //Refractored
+        //
+        //public async Task<ActionResult<IEnumerable<Make>>> GetMakes()
+        public async Task<IActionResult> Get()
+        {
+            //Refractored
+            //return await _context.Makes.ToListAsync();
+            var makes = await _unitOfWork.Makes.GetAll();
+            return Ok(makes);
+        }
+
+        // GET: api/Makes/5
+        [HttpGet("{id}")]
+        //Refractored
+        //public async Task<ActionResult<Make>> GetMake(int id)
+        public async Task<IActionResult> GetMake(int id)        
+        {
+            //Refractored
+            //var make = await _context.Makes.FindAsync(id);
+            var make = await _unitOfWork.Makes.Get(q => q.Id == id);
+
+            if (make == null)
+            {
+                return NotFound();
+            }
+            //Refractored
+            return Ok(make);
+        }
+
+        // PUT: api/Makes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMake(int id, Make make)
+        {
+            if (id != make.Id)
+            {
+                return BadRequest();
+            }
+            //Refractored
+            //_context.Entry(make).State = EntityState.Modified;
+            _unitOfWork.Makes.Update(make);
+
+            try
+            {
+                //Refractored
+                //await _context.SaveChangesAsync();
+                await _unitOfWork.Save(HttpContext);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                //Refractored
+                //if (!MakeExists(id))
+                if(!await MakeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Makes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Make>> PostMake(Make make)
+        {
+            //Refractor
+            //_context.Makes.Add(make);
+            //await _context.SaveChangesAsync();
+            await _unitOfWork.Makes.Insert(make);
+            await _unitOfWork.Save(HttpContext);
+
+            return CreatedAtAction("GetMake", new { id = make.Id }, make);
+        }
+
+        // DELETE: api/Makes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMake(int id)
+        {
+            //Refractored
+            //var make = await _context.Makes.FindAsync(id);
+            var make = await _unitOfWork.Makes.Get(q => q.Id == id);
+            if (make == null)
+            {
+                return NotFound();
+            }
+            //Refractored
+            //_context.Makes.Remove(make);
+            //await _context.SaveChangesAsync();
+            await _unitOfWork.Makes.Delete(id);
+            await _unitOfWork.Save(HttpContext);
+
+            return NoContent();
+        }
+
+        //Refractored
+        //private bool MakeExists(int id)
+        private async Task<bool> MakeExists(int id)
+        {
+            //Refractored
+            //return (_context.Makes?.Any(e => e.Id == id)).GetValueOrDefault();
+            var make = await _unitOfWork.Makes.Get(q => q.Id == id);
+            return make != null;
+        }
+    }
+}
